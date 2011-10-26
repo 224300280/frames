ba4c015d935e1766e05e344214a515f9f096bcab
Nathalia <nathaliacomth8@gmail.com>
2011-10-27 01:37:49 +0800

    Most Recent

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,16 @@
 class PagesController < ApplicationController
   def home
+  @title = "Frames"
   end
 
   def about
+  @title = "About Us"
+  
   end
 
   def map
+  @title = "Site Map"
+  
   end
 
 end
